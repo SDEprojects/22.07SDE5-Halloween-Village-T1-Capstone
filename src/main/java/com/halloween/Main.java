cc3a8d0bb5eebc0cfc43c67659be372377b031d2
kellichh <kellykchhe@gmail.com>
2022-10-12 18:04:08 -0400

    instructions and moved backstory and title to view, AB#323

--- src/main/java/com/halloween/Main.java
+++ src/main/java/com/halloween/Main.java
@@ -1,4 +1,16 @@
 package com.halloween;
 
+import com.halloween.controller.Game;
+
 public class Main {
+
+  public static void main(String[] args) {
+    Game game = new Game();
+
+    game.showTitle();
+    game.showBackstory();
+    game.showInstructions();
+
+  }
+
 }
