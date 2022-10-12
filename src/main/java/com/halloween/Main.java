dc7b708a4cbe1997b5f456c6f17e75d859454e33
kellichh <kellykchhe@gmail.com>
2022-10-12 16:39:45 -0400

    backstory completed, AB#317

--- src/main/java/com/halloween/Main.java
+++ src/main/java/com/halloween/Main.java
@@ -1,4 +1,23 @@
 package com.halloween;
 
 public class Main {
+
+  public static void main(String[] args) {
+    backstory();
+  }
+  private static void backstory() {
+    System.out.println(
+        "Welcome to Halloween Village! A community of ghosts, ghouls, goblins, \n"
+            + "and terrifying beasts from all over.\n"
+    );
+    System.out.println(
+        "It’s Halloween night. You must go trick or treating house-to-house \n"
+            + "collecting candy and useful items. Most citizens of Halloween Village \n"
+            + "are friendsy, but beware, lurking somewhere in the shadows is the scariest \n"
+            + "(and for sure rudest) villain of all time waiting for you! Should you \n"
+            + "manage to defeat this great evil, peace and prosperity will be restored to \n"
+            + "Halloween Village!"
+    );
+    System.out.println("\n\tGood Luck, you'll definitely need it...\n");
+  }
 }
